32a5948d1df00bfa3ecfc22bbb27bae2b2b8c181
Marton Piller <mrtn.pllr@gmail.com>
2019-06-26 21:53:16 +0200

    C++ exercise added

--- week-01/day-3/seconds_in_a_day/main.cpp
+++ week-01/day-3/seconds_in_a_day/main.cpp
@@ -3,17 +3,16 @@
 
 int main(int argc, char* args[]) {
 
-    int currentHours = 14;
-    int currentMinutes = 34;
-    int currentSeconds = 42;
-    int secondsInaDay = 86400;
-    int currentTimeSeconds = (currentHours * 60*60) + (currentMinutes * 60) + currentSeconds;
-
     // Write a program that prints the remaining seconds (as an integer) from a
     // day if the current time is represented by the variables
 
-    if(currentTimeSeconds ==  )
+    int currentHours = 14;
+    int currentMinutes = 34;
+    int currentSeconds = 42;
+    int currentTimeSeconds = (currentHours * 60*60) + (currentMinutes * 60) + currentSeconds;
+    int secondsInaDay = 86400;
 
+    std::cout << "Remaining seconds from the day: " << secondsInaDay - currentTimeSeconds << std::endl;
 
     return 0;
 }
