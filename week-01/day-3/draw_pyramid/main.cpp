d1af72d205507ae6bbf22b7bae235b55501bbd6d
Marton Piller <mrtn.pllr@gmail.com>
2019-06-27 16:14:21 +0200

    C++ exercise added

--- week-01/day-3/draw_pyramid/main.cpp
+++ week-01/day-3/draw_pyramid/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main (int argc, char* arcs[]) {
+int main(int argc, char *arcs[]) {
 
     // Write a program that reads a number from the standard input, then draws a
     // pyramid like this:
@@ -18,16 +18,16 @@ int main (int argc, char* arcs[]) {
     std::cout << "Give me a number!" << std::endl;
     std::cin >> num;
 
-    for (int i = 1; i <=num ; ++i) {
-
-        for (int j = 1; j <=i ; j++) {
+    for (int i = 1; i <= num; ++i) {
+        for (int k = (num-i); k >= 0; k--) {
+            std::cout << " ";
+        }
+        for (int j = 1; j <= (i*2) - 1; j++) {
             std::cout << "*";
         }
-        std::cout << std::endl;
 
+        std::cout << std::endl;
     }
 
-
-
-return 0;
+    return 0;
 }
