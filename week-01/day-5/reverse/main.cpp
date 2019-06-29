9909d070a0d27c2204056f8851235abb98ba28ea
Marton Piller <mrtn.pllr@gmail.com>
2019-06-29 12:28:56 +0200

    Add reverse exercise

--- week-01/day-5/reverse/main.cpp
+++ week-01/day-5/reverse/main.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <string>
+#include <bits/stdc++.h>
 
 int main(int argc, char* args[])
 
 {
-
-    // - Create an array variable named `aj`
-    //   with the following content: `[3, 4, 5, 6, 7]`
-    // - Reverse the order of the elements in `aj`
-    // - Print the elements of the reversed `aj`
-
     int aj[5] = {3, 4, 5, 6, 7};
 
-    for (int i = 0; i < sizeof(aj) / sizeof(aj[0]); ++i) {
-        for (int j = 0; j < sizeof(aj) / sizeof(aj[0]) ; ++j) {
-            for (int k = 0; k < sizeof(aj) / sizeof(aj[0]) ; ++k) {
-                aj[i] = aj [j];
-            }
-
-        }
+    for (int i = 0; i < (sizeof(aj) / sizeof(aj[0])) / 2; ++i) {
+        std::swap(aj[i], aj[(sizeof(aj) / sizeof(aj[0])) - i - 1]);
     }
 
-    std::cout << aj[1];
+    for (int j : aj) {
+        std:: cout << j << std::endl;
+    }
 
     return 0;
 }
