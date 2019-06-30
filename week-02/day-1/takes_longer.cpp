e02377777bed0f34a3f5dc88fa08e1c32665d9c9
Marton Piller <mrtn.pllr@gmail.com>
2019-06-30 21:37:11 +0200

    Fix style in takes longer exercise

--- week-02/day-1/takes_longer.cpp
+++ week-02/day-1/takes_longer.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char* args[])
 {
     std::string quote("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
-    quote.replace(21, 22, "always takes longer than ");
+    quote.replace(20, 0, "always takes longer than");
+    quote.replace(20, 0, " ");
     std::cout << quote << std::endl;
 
     return 0;
