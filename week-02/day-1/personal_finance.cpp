df6eca3c7363c2534a47d24873ea3f479374c734
Marton Piller <mrtn.pllr@gmail.com>
2019-07-01 18:12:06 +0200

    Fix personal_finance exercise

--- week-02/day-1/personal_finance.cpp
+++ week-02/day-1/personal_finance.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <algorithm>
 
-int spendingCalc(std::vector<int> vect);
+void spendingCalc(std::vector<int> vect);
 
-int main(int argc, char* args[])
+int main(int argc, char *args[])
 {
     /*We are going to represent our expenses in a list containing integers.
 
@@ -18,28 +18,30 @@ int main(int argc, char* args[])
     //500, 1000, 1250, 175, 800 and 120
     std::vector<int> spending = {500, 1000, 1250, 175, 800, 120};
 
-    std::cout << spendingCalc(spending);
-
-
-
+    spendingCalc(spending);
+    
     return 0;
 }
 
-int spendingCalc(std::vector<int> vect)
+void spendingCalc(std::vector<int> vect)
 {
     int sum = 0;
     int min;
     int max;
+    int average;
 
-    for (unsigned int i = 0; i < vect.size() ; ++i) {
+    for (unsigned int i = 0; i < vect.size(); ++i) {
         sum = sum + vect[i];
     }
 
-    min = *min_element(vect.begin(),vect.end());
+    min = *min_element(vect.begin(), vect.end());
+    max = *max_element(vect.begin(), vect.end());
 
-    max = *max_element(vect.begin(),vect.end());
+    average = sum / vect.size();
 
-    
-    return sum;
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Min: " << min << std::endl;
+    std::cout << "Max: " << max << std::endl;
+    std::cout << "Average: " << average << std::endl;
 }
 
