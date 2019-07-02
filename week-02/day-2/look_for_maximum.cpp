04d7a71a31f03a88a771be500dc1db5692621773
Marton Piller <mrtn.pllr@gmail.com>
2019-07-02 16:17:34 +0200

    Add look_for_maximum exercise

---
+++ week-02/day-2/look_for_maximum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+void biggestNumber(int arr[], int sizeOfArray);
+
+int main()
+{
+    // Create a program which first asks for a number++
+    // this number indicates how many integers we want to store in an array++
+    // and than asks for numbers till the user fills the array++
+    // It should print out the biggest number in the given array and the memory address of it+
+    int num;
+
+    std::cout << "Give me a number:";
+    std::cin >> num;
+
+    int array[num];
+
+    for (int i = 0; i < num; ++i) {
+        std::cout << "Give me more numbers!" << std::endl;
+        std::cin >> array[i];
+        std::cout << std::endl;
+    }
+
+    int sizeOfArray = sizeof(array) / sizeof(array[0]);
+
+    biggestNumber(array, sizeOfArray);
+
+    return 0;
+}
+
+void biggestNumber(int arr[], int sizeOfArray)
+{
+    int max = 0;
+
+    for (int i = 0; i < sizeOfArray; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    std::cout << max << std::endl << &max << std::endl;
+}
