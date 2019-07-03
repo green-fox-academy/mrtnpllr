c21700968923c6e2d88383224a8c768b44c6dd79
Marton Piller <mrtn.pllr@gmail.com>
2019-07-03 16:33:45 +0200

    Fix style in colored_box

--- week-02/day-3/colored_box.cpp
+++ week-02/day-3/colored_box.cpp
@@ -4,12 +4,6 @@
 
 void draw(SDL_Renderer *gRenderer)
 {
-    /*//choose color
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-    //create a rectangle
-    SDL_Rect fillRect = { 300, 300, 200, 200};
-    //draw rectangle
-    SDL_RenderFillRect( gRenderer, &fillRect );*/
     SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
     SDL_RenderDrawLine(gRenderer, 150, 150, 150, 450);
 
@@ -19,9 +13,6 @@ void draw(SDL_Renderer *gRenderer)
     SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
     SDL_RenderDrawLine(gRenderer, 450, 450, 450, 150);
 
-
     SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0xFF, 0xFF);
     SDL_RenderDrawLine(gRenderer, 150, 150, 450, 150);
-
-
 }
