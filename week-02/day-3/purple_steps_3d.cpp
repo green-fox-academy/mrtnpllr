057e457b67608156b0dc1e89154f4a1dd50531d2
Marton Piller <mrtn.pllr@gmail.com>
2019-07-03 17:52:09 +0200

    Add purple_steps_3d

---
+++ week-02/day-3/purple_steps_3d.cpp
@@ -0,0 +1,27 @@
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r4.png]
+// Pay attention for the outlines as well
+
+#include "draw.h"
+#include <iostream>
+
+void drawBoxes(int x, int y, int z, int w, SDL_Renderer *gRenderer);
+
+void draw(SDL_Renderer *gRenderer)
+{
+    for (int i = 1; i < 7; i++) {
+        drawBoxes((20 * i) * i/2 , (20 * i) * i/2, (20 * i), (20 * i), gRenderer);
+    }
+}
+
+void drawBoxes(int x, int y, int z, int w, SDL_Renderer *gRenderer)
+{
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 1);
+    SDL_Rect drawRect = {(x -1), (y - 1), z, w};
+    SDL_RenderDrawRect(gRenderer, &drawRect);
+
+    SDL_SetRenderDrawColor(gRenderer, 177, 69, 244, 1);
+    SDL_Rect fillRect = {x, y, (z - 2), (w - 2)};
+    SDL_RenderFillRect(gRenderer, &fillRect);
+}
