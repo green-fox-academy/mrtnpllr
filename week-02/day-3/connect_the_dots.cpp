047dcbafc320719b7050ae2104ebe1bccbe8940e
Marton Piller <mrtn.pllr@gmail.com>
2019-07-04 15:36:40 +0200

    Update CmakeList

--- week-02/day-3/connect_the_dots.cpp
+++ week-02/day-3/connect_the_dots.cpp
@@ -1,7 +1,7 @@
 // Create a function that takes 1 parameter:
 // An array of {x, y} points
 // and connects them with green lines.
-// Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+// Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}, {10, 10}}
 // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}}
 
 #include "draw.h"
