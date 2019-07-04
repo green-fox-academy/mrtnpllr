a53196e0bff03c89319a1a1d427d9aa413906212
Marton Piller <mrtn.pllr@gmail.com>
2019-07-04 14:56:38 +0200

    Update line_play

--- week-02/day-3/line_play.cpp
+++ week-02/day-3/line_play.cpp
@@ -4,13 +4,14 @@ void draw(SDL_Renderer *gRenderer)
 {
     SDL_SetRenderDrawColor(gRenderer, 60, 184, 120, 1);
     int widthHeight = 600;
+    int lineNum = 12;
 
-    for (int i = 0; i <= 12; i++) {
-        SDL_RenderDrawLine(gRenderer, 20 + (i * widthHeight / 12), 0, widthHeight, 20 + (i * widthHeight / 12));
+    for (int i = 0; i <= lineNum; i++) {
+        SDL_RenderDrawLine(gRenderer, 20 + (i * widthHeight / lineNum), 0, widthHeight, 20 + (i * widthHeight / lineNum));
     }
 
     SDL_SetRenderDrawColor(gRenderer, 150, 50, 150, 1);
-    for (int j = 0; j <= 12; j++) {
-        SDL_RenderDrawLine(gRenderer, 0, 20 + (j * widthHeight / 12), 20 + (j * widthHeight / 12), widthHeight);
+    for (int j = 0; j <= lineNum; j++) {
+        SDL_RenderDrawLine(gRenderer, 0, 20 + (j * widthHeight / lineNum), 20 + (j * widthHeight / lineNum), widthHeight);
     }
 }
