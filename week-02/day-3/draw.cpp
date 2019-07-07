0cb7c653ca802b92238a62bd5c424e56477388ad
Marton Piller <mrtn.pllr@gmail.com>
2019-07-07 15:56:19 +0200

    Add triangles exercise

--- week-02/day-3/draw.cpp
+++ week-02/day-3/draw.cpp
@@ -1,4 +1,11 @@
 #include "draw.h"
 
-void draw(SDL_Renderer* gRenderer) {
+void draw(SDL_Renderer *gRenderer)
+{
+    //choose color
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+    //draw line
+    for (int i = 0; i < 100; ++i) {
+        SDL_RenderDrawLine(gRenderer, 100 * i, 0, 200, 200);
+    }
 }
---
+++ week-02/day-3/triangles.cpp
@@ -0,0 +1,17 @@
+#include "draw.h"
+#include <math.h>
+
+void draw(SDL_Renderer *gRenderer)
+{
+    int widthHeight = 600;
+    int sideOfTriangle = 60;
+    int heightOfTriangle = 60; //Apparently using the math formula makes deformed triangles... (sideOfTriangle * sqrt(3) / 2 -> avoid using this)
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 1);
+
+    for (int i = 0; i < widthHeight / heightOfTriangle; ++i) {
+        SDL_RenderDrawLine(gRenderer, (widthHeight / 2) + sideOfTriangle/ 2 * i, heightOfTriangle * i, sideOfTriangle * i, widthHeight);
+        SDL_RenderDrawLine(gRenderer, (widthHeight / 2) - sideOfTriangle/ 2 * i, heightOfTriangle * i, widthHeight - (sideOfTriangle * i) , widthHeight);
+        SDL_RenderDrawLine(gRenderer, (widthHeight / 2) - sideOfTriangle/ 2 * i, heightOfTriangle * i, (widthHeight / 2) + sideOfTriangle / 2 * i, heightOfTriangle * i);
+    }
+}
