ae6c08205897a1e3802159f6291bc1b43724e274
Marton Piller <mrtn.pllr@gmail.com>
2019-07-09 14:24:38 +0200

    Update log exercise

--- week-03/day-1/logs.cpp
+++ week-03/day-1/logs.cpp
@@ -9,30 +9,44 @@
 // Write a function that returns an array with the unique IP adresses.
 // Write a function that returns the GET / POST request ratio.
 
-std::vector<std::string> getIpAdresses(std::string filename);
 bool contains(std::vector<std::string> vectorIn, std::string checker);
-std::vector<std::string> unique(std::vector<std::string> inputVector);
-std::vector<std::string> getPost (std::string filename);
-double getPostRatio (std::vector<std::string> vecIn);
 
-//main Function
+std::vector<std::string> unique(std::vector<std::string> inputVector);
+
+std::vector<std::string> getIpAddresses(std::string filename);
+
+std::vector<std::string> getPost(std::string filename);
+
+double getPostRatio(std::vector<std::string> vecIn);
+
+//main function
 int main()
 {
+    char choice;
+
     std::string path = "../log.txt";
-    std::vector<std::string> tmpV = unique(getIpAdresses(path));
-    for (int i = 0; i < tmpV.size(); ++i) {
-        //std::cout << tmpV[i] << std::endl;
+    std::vector<std::string> tmpV = unique(getIpAddresses(path));
+
+    std::cout << "The number of unique IP addresses: " << tmpV.size() << std::endl;
+    std::cout << "The GET/POST ratio is: " << getPostRatio(getPost(path)) << std::endl;
+    std::cout << "Do you wish to see the individual IP addresses? (Y/N)" << std::endl;
+
+    std::cin >> choice;
+
+    if (choice == 'Y' || choice == 'y') {
+        for (int i = 0; i < tmpV.size(); ++i) {
+            std::cout << tmpV[i] << std::endl;
+        }
+    } else if (choice == 'N' || choice == 'n') {
+        std::cout << "Goodbye!" << std::endl;
+    } else {
+        std::cout << "Invalid input!" << std::endl;
     }
-
-    std::cout << std::endl;
-    //std::cout << tmpV.size();
-
-    std::cout << getPostRatio(getPost(path));
 
     return 0;
 }
 
-//contains Function
+//contains function
 bool contains(std::vector<std::string> vectorIn, std::string checker)
 {
     for (int i = 0; i < vectorIn.size(); ++i) {
@@ -55,9 +69,8 @@ std::vector<std::string> unique(std::vector<std::string> inputVector)
     return container;
 }
 
-
-//getIpAdresses Function
-std::vector<std::string> getIpAdresses(std::string filename)
+//getIpAddresses function
+std::vector<std::string> getIpAddresses(std::string filename)
 {
     std::fstream openFile(filename, std::ios::in);
     std::string readInData;
@@ -80,11 +93,11 @@ std::vector<std::string> getIpAdresses(std::string filename)
 }
 
 //getPost function
-std::vector<std::string> getPost (std::string filename)
+std::vector<std::string> getPost(std::string filename)
 {
     std::string readInData;
     std::string data;
-    std::vector<std::string> GetPost;
+    std::vector<std::string> getPostVector;
 
     std::fstream openFile(filename, std::ios::in);
 
@@ -95,22 +108,17 @@ std::vector<std::string> getPost (std::string filename)
             for (int i = 0; i < 12; ++i) {
                 std::getline(delim, data, ' ');
             }
-            GetPost.push_back(data);
+            getPostVector.push_back(data);
         }
     }
 
     openFile.close();
-    for (int j = 0; j < GetPost.size(); ++j) {
-        //std::cout << GetPost[j] << std::endl;
-    }
 
-    //std::cout << GetPost.size();
-
-    return GetPost;
+    return getPostVector;
 }
 
 //getPostRatio function
-double getPostRatio (std::vector<std::string> vecIn)
+double getPostRatio(std::vector<std::string> vecIn)
 {
     double counter1 = 0;
     double counter2 = 0;
@@ -123,11 +131,7 @@ double getPostRatio (std::vector<std::string> vecIn)
         }
     }
 
-    std::cout << counter1 << " " << counter2 << std::endl;
-
-    std::cout << counter1 / counter2 << std::endl;
-
-    return counter1/counter2;
+    return counter1 / counter2;
 }
 
 //Ha ebben a függvényben double typeot használok, jól visszaadja, de ha intet, akkor nem. Miért? A függvény doublet ad vissza.
