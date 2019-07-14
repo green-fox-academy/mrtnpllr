3c6e2940367c6e7683d78f4b67d0dd4344300e8b
Marton Piller <mrtn.pllr@gmail.com>
2019-07-14 17:50:33 +0200

    Add strings2 exercise

--- week-03/day-5/recursion/strings2.cpp
+++ week-03/day-5/recursion/strings2.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
+#include <string>
 
 //Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+std::string convertString(std::string input, int i);
+
+int main()
+{
+    std::string test = "xxxx 24323x sdfs sdfsdflkkv xx ynxnyly xyxnyxldsfdfksdfklsdlkfsdlfk";
+    int counter = 0;
+
+    std::cout << convertString(test, counter);
+
+    return 0;
+}
+
+std::string convertString(std::string input, int i)
+{
+    if (i == input.length()) {
+        return input;
+    } else if (input[i] == 'x') {
+        input.erase(i, 1);
+        return convertString(input, i);
+    } else {
+        return convertString(input, i + 1);
+    }
+}
