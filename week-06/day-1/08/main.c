0c1d60531dcb41f789c097efda60656b26c36654
Marton Piller <mrtn.pllr@gmail.com>
2019-08-05 15:20:52 +0200

    Add old enough exercise

--- week-06/day-1/08/main.c
+++ week-06/day-1/08/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int can_buy_alcohol(int age);
 
 int main()
 {
-    printf("Hello, World!\n");
+    // Create a program which asks for the age of the user and stores it
+    // Create a function which takes that age as a parameter and returns if the user is
+    // old enough to buy himself alcohol in Hungary
+
+    int age_of_user;
+
+    printf("Enter your age:\n");
+    scanf("%d", &age_of_user);
+
+    printf("%d", can_buy_alcohol(age_of_user));
+
     return 0;
+}
+
+int can_buy_alcohol(int age)
+{
+    if (age >= 18) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
