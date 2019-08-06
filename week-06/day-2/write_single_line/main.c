d54fa7c7ed90c167f237ae4ea85d244ad5ebb45f
Marton Piller <mrtn.pllr@gmail.com>
2019-08-06 20:07:08 +0200

    Add write_single_line

---
+++ week-06/day-2/write_single_line/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+// Open a file called "my-file.txt"
+// Write your name in it as a single line
+
+int write_txt_file(char *path);
+
+int main()
+{
+    char path[] = "../my_file.txt";
+
+    printf("The return of the write_txt_file fnct: %d", write_txt_file(path));
+
+    return 0;
+}
+
+int write_txt_file(char *path)
+{
+    FILE *write = fopen(path, "w");
+    if (write == NULL) {
+        return -1;
+    }
+
+    char my_name[256] = "Marton Piller";
+
+    fputs(my_name, write);
+
+    return 1;
+}
