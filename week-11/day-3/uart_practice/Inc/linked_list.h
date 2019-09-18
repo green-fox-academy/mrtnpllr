60f21aa4e7119c7af1ff88ac53b249e108fee9c6
Piller Marotn <mrtn.pllr@gmail.com>
2019-09-18 16:26:59 +0200

    Add uart practice

---
+++ week-11/day-3/uart_practice/Inc/linked_list.h
@@ -0,0 +1,18 @@
+#ifndef LINKED_LIST_LINKED_LIST_H
+#define LINKED_LIST_LINKED_LIST_H
+
+
+#include "cmsis_os.h"
+
+typedef struct node{
+	char* data;
+	struct node* next;
+}node_t;
+
+void linked_list_create();
+void linked_list_deallocate(node_t *head);
+void linked_list_push_back(node_t **head, char* data);
+void linked_list_push_front(node_t **head, char* data);
+void linked_list_pop_back(node_t **head);
+
+#endif
